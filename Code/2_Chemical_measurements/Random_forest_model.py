a9e1729a37ccaa263ad42325d74f84060cb79127
Jrchapp3 <jrchapp3@ncsu.edu>
2024-07-26 15:54:40 -0400

    Update code structure

--- Code/2_Chemical_measurements/Random_forest_model.py
+++ Code/2_Chemical_measurements/Random_forest_model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import numpy as np
 import pickle
@@ -12,27 +11,27 @@ np.random.seed(17)
 
 # Read in all inputs
 # Full input
-train_x = pd.read_pickle("Data_inputs/train_x")
-train_y = pd.read_pickle("Data_inputs/train_y")
-test_x = pd.read_pickle("Data_inputs/test_x")
-test_y = pd.read_pickle("Data_inputs/test_y")
+train_x = pd.read_pickle("Data_inputs/2_Chemical_measurements/train_x")
+train_y = pd.read_pickle("Data_inputs/2_Chemical_measurements/train_y")
+test_x = pd.read_pickle("Data_inputs/2_Chemical_measurements/test_x")
+test_y = pd.read_pickle("Data_inputs/2_Chemical_measurements/test_y")
 
 # PCA
-train_x_pca = pd.read_pickle("Data_inputs/train_x_pca")
-test_x_pca = pd.read_pickle("Data_inputs/test_x_pca")
+train_x_pca = pd.read_pickle("Data_inputs/2_Chemical_measurements/train_x_pca")
+test_x_pca = pd.read_pickle("Data_inputs/2_Chemical_measurements/test_x_pca")
 
 # LASSO
-train_x_lasso = pd.read_pickle("Data_inputs/train_x_lasso")
-test_x_lasso= pd.read_pickle("Data_inputs/test_x_lasso")
+train_x_lasso = pd.read_pickle("Data_inputs/2_Chemical_measurements/train_x_lasso")
+test_x_lasso= pd.read_pickle("Data_inputs/2_Chemical_measurements/test_x_lasso")
 
 # Create dictionaries containing full input, PCA-reduced input, and Lasso-reduced input to iterate through 
 train_input_dict = {'Full': train_x, 'PCA': train_x_pca, 'Lasso': train_x_lasso}
 test_input_dict = {'Full': test_x, 'PCA': test_x_pca, 'Lasso': test_x_lasso}
 
 # Save dictionaries for future use
-with open('Data_inputs/train_input_dict.pkl', 'wb') as f:
+with open('Data_inputs/2_Chemical_measurements/train_input_dict.pkl', 'wb') as f:
     pickle.dump(train_input_dict, f)
-with open('Data_inputs/test_input_dict.pkl', 'wb') as f:
+with open('Data_inputs/2_Chemical_measurements/test_input_dict.pkl', 'wb') as f:
     pickle.dump(test_input_dict, f)
 
 # Convert dictionary keys to a list
@@ -85,94 +84,5 @@ for i in range(len(train_input_dict)):
 results_rf_df 
 
 # Save model comparisons to csv
-file_name = f'Models/rf/rf_model_comparison.csv'
-=======
-import pandas as pd
-import numpy as np
-import pickle
-from sklearn.ensemble import RandomForestRegressor
-import time
-from sklearn.metrics import root_mean_squared_error
-import os
-
-# Set seed
-np.random.seed(17)
-
-# Read in all inputs
-# Full input
-train_x = pd.read_pickle("Data_inputs/train_x")
-train_y = pd.read_pickle("Data_inputs/train_y")
-test_x = pd.read_pickle("Data_inputs/test_x")
-test_y = pd.read_pickle("Data_inputs/test_y")
-
-# PCA
-train_x_pca = pd.read_pickle("Data_inputs/train_x_pca")
-test_x_pca = pd.read_pickle("Data_inputs/test_x_pca")
-
-# LASSO
-train_x_lasso = pd.read_pickle("Data_inputs/train_x_lasso")
-test_x_lasso= pd.read_pickle("Data_inputs/test_x_lasso")
-
-# Create dictionaries containing full input, PCA-reduced input, and Lasso-reduced input to iterate through 
-train_input_dict = {'Full': train_x, 'PCA': train_x_pca, 'Lasso': train_x_lasso}
-test_input_dict = {'Full': test_x, 'PCA': test_x_pca, 'Lasso': test_x_lasso}
-
-# Save dictionaries for future use
-with open('Data_inputs/train_input_dict.pkl', 'wb') as f:
-    pickle.dump(train_input_dict, f)
-with open('Data_inputs/test_input_dict.pkl', 'wb') as f:
-    pickle.dump(test_input_dict, f)
-
-# Convert dictionary keys to a list
-keys = list(train_input_dict.keys())
-
-# Initialize a DataFrame to store results
-results_rf_df = pd.DataFrame(columns=["Training RMSE", "Test RMSE", "Time Taken"])
-
-# Iterate through inputs and run random forest model
-for i in range(len(train_input_dict)):
-    # Subset to relevant input
-    key = keys[i]
-    df_train = train_input_dict[key]
-    df_test = test_input_dict[key]
-
-    # Model parameters
-    rf_model = RandomForestRegressor(n_estimators=10000, random_state=17)
-
-    # Run and time model
-    start_time = time.time()
-    rf_model.fit(df_train, train_y)
-    end_time = time.time()
-    time_taken = end_time - start_time
-
-    # Extract variable importance
-    importances = rf_model.feature_importances_
-    var_imp_rf_injury = pd.DataFrame({"Feature": df_train.columns, "Importance": importances})
-    var_imp_rf_injury = var_imp_rf_injury.sort_values("Importance", ascending=False)
-
-    # Plot variable importance
-    # plt.figure(figsize=(10, 6))
-    # var_imp_rf_injury.plot(kind="bar", x="Feature", y="Importance")
-    # plt.title("Variable Importance Plot")
-    # plt.xlabel("Feature")
-    # plt.ylabel("Importance")
-    # plt.show()
-
-    # Get training data RMSE 
-    train_pred = rf_model.predict(df_train)
-    train_rmse = root_mean_squared_error(train_y, train_pred)
-
-    # Apply model to test set
-    test_pred = rf_model.predict(df_test)
-    test_rmse = root_mean_squared_error(test_y, test_pred)
-
-    # Store results in DataFrame
-    results_rf_df .loc[key] = [train_rmse, test_rmse, time_taken]
-
-# Print final results
-results_rf_df 
-
-# Save model comparisons to csv
-file_name = f'Models/rf/rf_model_comparison.csv'
->>>>>>> b005bad21d7ce7904bef7b61cc2c43d41952fe1b
+file_name = f'Models/2_Chemical_measurements/rf/rf_model_comparison.csv'
 results_rf_df.to_csv(file_name, index=False)
