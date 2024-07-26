cfde676db1947c238f74f3a08696c36f673e598a
Jrchapp3 <jrchapp3@ncsu.edu>
2024-07-26 12:51:11 -0400

    try push again

--- Code/Simulated_example.py
+++ Code/Simulated_example.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import pysr
-import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt 
 
 # https://astroautomata.com/PySR/
 # Set random seed for reproducibility
